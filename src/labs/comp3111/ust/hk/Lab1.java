a01f7294d846c06cff98776bfa9c3837ea5e92fe
ASUS <ASUS@10.89.209.133>
2018-02-06 18:03:42 +0800

    Added a congraduation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -5,5 +5,6 @@ public class Lab1 {
 
  	public static void main(String[] args) {
  		System.out.println("Lab 1: Introduction to Git and Github");
+ 		System.out.println("Congraduation! You learned the Git Basics");
  	}
 }
